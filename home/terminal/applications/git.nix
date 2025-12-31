f59299197fda08a1b143fb1943ffdd77bb7d6f39
Luis Eduardo M. <luiseduardomc019@gmail.com>
2025-12-31 16:46:06 -0300

    updating nixos configuration

--- home/terminal/applications/git.nix
+++ home/terminal/applications/git.nix
@@ -5,6 +5,11 @@
     enable = true;
 
     settings = {
+      user = {
+        name = "Luis Eduardo M.";
+        email = "luiseduardomc019@gmail.com";
+      };
+
       extraConfig = {
         init.defaultBranch = "master";
         core.editor = "nvim";
