fa6825bc78216d41d769f704018bdc974138e39d
Luis Eduardo M. <luiseduardomc019@gmail.com>
2026-01-03 01:19:55 -0300

    new

--- home/softwares/hyprland/windowrule.nix
+++ home/softwares/hyprland/windowrule.nix
@@ -4,24 +4,21 @@
   wayland.windowManager.hyprland = {
     settings = {
       windowrulev2 = [
-				"workspace 5 silent,class:^(org.qbittorrent.qBittorrent)$"
-				"workspace 6 silent,class:^(discord)$"
+        "workspace 6 silent,class:^(org.qbittorrent.qBittorrent)$"
+        "workspace 5 silent,class:^(discord)$"
 
-				"workspace 4 silent,class:^(IVPN)$"
-				"move 1590 439,class:^(IVPN)$"
+        "workspace 4 silent,class:^(IVPN)$"
+        "move 1590 439,class:^(IVPN)$"
 
-				"workspace 4 silent,class:^(Spotify)$"
-				"float,class:^(Spotify)$"
-				"size 1550 950,class:^(Spotify)$"
-				"move 20 90,class:^(Spotify)$"
+        "workspace 4 silent,class:^(Spotify)$"
+        "float,class:^(Spotify)$"
 
-				"opacity 0.0 override, class:^(xwaylandvideobridge)$"
-				"noanim, class:^(xwaylandvideobridge)$"
-				"noinitialfocus, class:^(xwaylandvideobridge)$"
-				"maxsize 1 1, class:^(xwaylandvideobridge)$"
-				"noblur, class:^(xwaylandvideobridge)$"
+        "opacity 0.0 override, class:^(xwaylandvideobridge)$"
+        "noanim, class:^(xwaylandvideobridge)$"
+        "noinitialfocus, class:^(xwaylandvideobridge)$"
+        "maxsize 1 1, class:^(xwaylandvideobridge)$"
+        "noblur, class:^(xwaylandvideobridge)$"
       ];
     };
   };
 }
-
